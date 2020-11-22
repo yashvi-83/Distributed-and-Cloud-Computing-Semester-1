6079d3cff373e7969906e233dd3c955f24b0e882
NirvitiLalooa <62436374+NirvitiLalooa@users.noreply.github.com>
2020-11-22 16:23:23 +0400

    Update InterfaceServer.java

--- Chat Server RMI/src/server/InterfaceServer.java
+++ Chat Server RMI/src/server/InterfaceServer.java
@@ -9,30 +9,30 @@ import java.util.Vector;
 
 
 public interface InterfaceServer extends Remote{
-    //cette fonction pour distribuer un message vers tous clients connectées
+    //Function to broadcast a message to all connected clients
     void broadcastMessage(String message,List<String> list) throws RemoteException;
     
-    //cette fonction pour distribuer un fichier partagée vers tous clients connectes
+    //Function to broadcast a file to all connected clients
     void broadcastMessage(ArrayList<Integer> inc,List<String> list,String filename) throws RemoteException;
     
-    //cette fonction pour recupere le nom des clients connectes
+    //Function to get list of connected clients
     Vector<String> getListClientByName(String name) throws RemoteException;
     
-    //cette fonction pour ajouter un client connectées a la liste des clients connectées
+    //Function to add new connected client to the list of connected clients
     void addClient(InterfaceClient client) throws RemoteException;
     
-    //cette fonction pour blocker un client d"envoyer un message, mais il peut recu les messages
+    //Function to block a user from sending message but can receive other messages
     void blockClient(List<String> clients) throws RemoteException;
     
-    //cette fonction pour supprimer totalement une liste des clients de chat (kick-out)
+    //Function to remove list of connected clients
     void removeClient(List<String> clients) throws RemoteException;
     
-    //cette fonction pour supprimer totalement un seul client de chat (kick-out)
+    //Function to remove one client
     void removeClient(String clients) throws RemoteException;
     
-    //cette fonction pour activer un client dans chat, d'apres etre dans le cas de "block"
+    //Function to activate a client after being "blocked"
     void reactiveClient(List<String> clients) throws RemoteException;
     
-    //cette fonction pour verfifier est que un username existe deja dans le serveur ou non, car username est l'identificateur sur chat
+    //Function to check if a username already exists on the server
     boolean checkUsername(String username) throws RemoteException;
 }
