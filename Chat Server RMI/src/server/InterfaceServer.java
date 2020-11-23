e2367fc53571e60056b9ce93f2db226730dd197d
Mouzammil <jowaheermouz@gmail.com>
2020-11-23 20:08:06 +0400

    Update InterfaceServer.java

--- Chat Server RMI/src/server/InterfaceServer.java
+++ Chat Server RMI/src/server/InterfaceServer.java
@@ -8,31 +8,20 @@ import java.util.List;
 import java.util.Vector;
 
 
-public interface InterfaceServer extends Remote{
-    //Function to broadcast a message to all connected clients
+public interface InterfaceServer extends Remote{    
+    //distributes a message to all connected clients
     void broadcastMessage(String message,List<String> list) throws RemoteException;
     
-    //Function to broadcast a file to all connected clients
-    void broadcastMessage(ArrayList<Integer> inc,List<String> list,String filename) throws RemoteException;
+    //distributes a shared file to all connected clients
+//    void broadcastMessage(ArrayList<Integer> inc,List<String> list,String filename) throws RemoteException;
     
-    //Function to get list of connected clients
+    // retrieves the name of the connected clients
     Vector<String> getListClientByName(String name) throws RemoteException;
     
-    //Function to add new connected client to the list of connected clients
+    //this function to adds a connected client to the list of connected clients
     void addClient(InterfaceClient client) throws RemoteException;
     
-    //Function to block a user from sending message but can receive other messages
-    void blockClient(List<String> clients) throws RemoteException;
     
-    //Function to remove list of connected clients
-    void removeClient(List<String> clients) throws RemoteException;
-    
-    //Function to remove one client
-    void removeClient(String clients) throws RemoteException;
-    
-    //Function to activate a client after being "blocked"
-    void reactiveClient(List<String> clients) throws RemoteException;
-    
-    //Function to check if a username already exists on the server
+    //verifies is whether a username already exists in the server or not, because username is the identifier on chat
     boolean checkUsername(String username) throws RemoteException;
 }
